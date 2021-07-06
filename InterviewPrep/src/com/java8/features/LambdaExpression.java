ed62f4883b63ac1e5066b77991ccffe42995e8e0
Aditya <adityapujari333@outlook.com>
2021-07-06 08:11:12 +0530

    Deleted Unwanted FIles

---
+++ InterviewPrep/src/com/java8/features/LambdaExpression.java
@@ -0,0 +1,12 @@
+package com.java8.features;
+
+/*Lambda expression is
+	Nameless
+	No return type
+	No access modifiers
+*/
+public class LambdaExpression {
+	public static void main(String[] args) {
+
+	}
+}
--- InterviewPrep/src/com/javaversion/features/AbstractClass.java
+++
@@ -1,5 +0,0 @@
-package com.javaversion.features;
-
-public abstract class AbstractClass {
-	abstract void mymethod();
-}
