f35efa2453d21f361ea93705e09e2cea8ca172df
Aditya Pujari <adityapujari333@gmail.com>
2021-07-07 23:35:22 +0530

    lamda learnt

--- InterviewPrep/src/com/java8/features/LambdaExpression.java
+++ InterviewPrep/src/com/java8/features/LambdaExpression.java
@@ -4,9 +4,35 @@ package com.java8.features;
 	Nameless
 	No return type
 	No access modifiers
+	LE can be called via Functional interface
 */
+/******************
+ * Examples of functional interface
+ * Runnable = run()
+ * Comparable = compareTo()
+ * Comparator = compare()
+ * ActionListner = actionPerformed()
+ * Callable = call()
+ */
 public class LambdaExpression {
 	public static void main(String[] args) {
-
+		LambdaExpression le = new LambdaExpression();
+		le.m1();
 	}
+	
+	public void m1() {
+		System.out.println("hello");
+	}
+	//() -> System.out.println("hello");
+	
+	public void m2(int a,int b) {
+		System.out.println(a+b);
+	}
+	//() -> System.out.println(a+b);
+	
+	public int squareIt(int n) {
+		return n*n;
+	}
+	
+	n -> return n*n; 
 }
